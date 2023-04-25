cb8c75d81bb434b1c96b70f3d618143b929f80af
danmilldev <126679134+danmilldev@users.noreply.github.com>
2023-04-25 18:20:05 +0200

    some more commenting on the functions

--- script.js
+++ script.js
@@ -2,10 +2,13 @@ let roundNumber = 5;
 
 function chooseSymbol(selectionNumber)
 {
+    //if the random number is 1 it returns "rock" if 2 returns "paper" if 3 returns "scissor"
     let symbol = selectionNumber == 1 ? "Rock" : selectionNumber == 2 ? "Paper" : "Scissor";
     return symbol;
 }
 
+//will get a random number stored into choiceNumber
+//and returns the choice as a string decided with the chooseSymbol() Function
 function getComputerChoice() 
 {
     let choice = "";
@@ -23,6 +26,7 @@ function playRound(playerSelection,computerSelection)
     //then it checks the playersymbol against the computer one
     //and returns if the player wins or not
 
+    //the result variable to store the game result in it
     let result = "";
 
     if(playerSelection == "rock" && computerSelection == "Scissor")
